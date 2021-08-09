dafcc7b1e0fabeeccf0c9957f04f93637129ebf3
RBerezko <VTB4037828@vtb.ru>
2021-08-09 21:13:58 +0300

    произведен рефакторинг нового варианта отчетов (2 ч.)

--- src/main/java/excel_parser/SecondExcelReport.java
+++ src/main/java/excel_parser/SecondExcelReport.java
@@ -1,51 +1,46 @@
 package excel_parser;
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.util.*;
 
-import static excel_parser.ExcelReader.getPredictCellValue;
-import static excel_parser.ExcelReader.getWorkBook;
 import static excel_parser.ExcelWriter.*;
-import static excel_parser.ExcelWriterUtil.*;
 
 public class SecondExcelReport {
 
     public static void createSecondReport(XSSFWorkbook workBook) {
-        List<Map<String, Row>> firstReportRowsList = getFilteredMap();
-        XSSFSheet sheet = workBook.createSheet("Срез Ева без оператора");
-        generateSecondReport(workBook, firstReportRowsList, sheet);
+        List<Map<String, Row>> firstReportRowsList = FirstExcelReport.getFirstReportRowsList();
+        XSSFSheet secondReportSheet = workBook.createSheet("Срез Ева без оператора");
+        generateSecondReport(workBook, firstReportRowsList, secondReportSheet);
     }
 
-    private static void generateSecondReport(XSSFWorkbook workBook, List<Map<String, Row>> firstReportRowsList, XSSFSheet sheet) {
-        List<Map<String, Row>> rowsWithoutClearAnswersList = filterClearAnswerRows();
-        List<String> predictList = getPredictList();
+    private static void generateSecondReport(XSSFWorkbook workBook, List<Map<String, Row>> firstReportRowsList, XSSFSheet createdSheet) {
         List<Map<String, Row>> secondReportRowsList = new ArrayList<>();
+        List<Map<String, Row>> rowsWithoutClearAnswersList = ExcelReportsUtil.getRowsListWithoutAnswerCell(workBook.getSheetAt(DATA_SHEET_INDEX));
+        List<String> predictList = getCategoriesList();
 
         for (String predict : predictList) {
-            List<Map<String, Row>> rowsWithAnswer = filterListForPredict(rowsWithoutClearAnswersList, predict);
+            List<Map<String, Row>> categoryRowsList = ExcelReportsUtil.createCategoryRowsList(rowsWithoutClearAnswersList, predict);
 
-            if (rowsWithAnswer.size() >= 20) {
-                List<Map<String, Row>> uniqueRowsCategoryList = createUniqueRowsCategoryList(firstReportRowsList, rowsWithAnswer);
-                List<Map<String, Row>> shuffledTenRows = getShuffledTenRows(uniqueRowsCategoryList);
+            if (categoryRowsList.size() >= 20) {
+                List<Map<String, Row>> uniqueRowsList = ExcelReportsUtil.createUniqueRowsList(firstReportRowsList, categoryRowsList);
+                List<Map<String, Row>> shuffledTenRows = getShuffledTenRows(uniqueRowsList);
                 secondReportRowsList.addAll(shuffledTenRows);
-            } else if (rowsWithAnswer.size() >= 10) {
-                List<Map<String, Row>> uniqueRowsCategoryList = createUniqueRowsCategoryList(firstReportRowsList, rowsWithAnswer);
-                if (uniqueRowsCategoryList.size() < 10) {
-                    secondReportRowsList.addAll(uniqueRowsCategoryList);
+            } else if (categoryRowsList.size() >= 10) {
+                List<Map<String, Row>> uniqueRowsList = ExcelReportsUtil.createUniqueRowsList(firstReportRowsList, categoryRowsList);
+                if (uniqueRowsList.size() < 10) {
+                    secondReportRowsList.addAll(uniqueRowsList);
                 } else {
-                    List<Map<String, Row>> shuffledTenRows = getShuffledTenRows(rowsWithAnswer);
+                    List<Map<String, Row>> shuffledTenRows = getShuffledTenRows(categoryRowsList);
                     secondReportRowsList.addAll(shuffledTenRows);
                 }
             } else {
-                secondReportRowsList.addAll(rowsWithAnswer);
+                secondReportRowsList.addAll(categoryRowsList);
             }
         }
-        createReport(workBook, sheet, secondReportRowsList);
+        createReport(workBook, createdSheet, secondReportRowsList);
     }
 
     private static List<Map<String, Row>> getShuffledTenRows(List<Map<String, Row>> rowsWithoutAnswerList) {
@@ -55,46 +50,5 @@ public class SecondExcelReport {
             categoryList.add(rowsWithoutAnswerList.get(i));
         }
         return categoryList;
-    }
-
-    private static List<Map<String, Row>> createUniqueRowsCategoryList(List<Map<String, Row>> firstReportRowsList, List<Map<String, Row>> rowsWithoutAnswerList) {
-        List<Map<String, Row>> uniqueRowsCategoryList = new ArrayList<>();
-        for (int i = 0; i < rowsWithoutAnswerList.size(); i++) {
-            for (Map<String, Row> firstReportRowMap : firstReportRowsList) {
-                if (!rowsWithoutAnswerList.get(i).entrySet().containsAll(firstReportRowMap.entrySet())) {
-                    Map<String, Row> tempMap = new HashMap<>(rowsWithoutAnswerList.get(i));
-                    uniqueRowsCategoryList.add(tempMap);
-                }
-            }
-        }
-        return uniqueRowsCategoryList;
-    }
-
-    private static List<Map<String, Row>> filterClearAnswerRows() {
-        List<Map<String, Row>> rowsWithoutClearAnswerList = new ArrayList<>();
-        XSSFWorkbook workbook = getWorkBook();
-        Sheet dataSheet = workbook.getSheetAt(DATA_SHEET_INDEX);
-
-        Iterator<Row> rowIterator = dataSheet.iterator();
-        Row headersRow = rowIterator.next();
-        Map<String, Row> headersMap = createHeadersMap(headersRow);
-        rowsWithoutClearAnswerList.add(headersMap);
-        while (rowIterator.hasNext()) {
-            Row currentRow = rowIterator.next();
-            Cell answerCell = currentRow.getCell(ANSWER_CELL_COLUMN_INDEX);
-            if (answerCell != null) {
-                String predictCellValue = getPredictCellValue(currentRow);
-                Map<String, Row> rowMap = new HashMap<>();
-                rowMap.put(predictCellValue, currentRow);
-                rowsWithoutClearAnswerList.add(rowMap);
-            }
-        }
-        return rowsWithoutClearAnswerList;
-    }
-
-    static Map<String, Row> createHeadersMap(Row headersRow) {
-        Map<String, Row> headersMap = new HashMap<>();
-        headersMap.put("headers", headersRow);
-        return headersMap;
     }
 }
